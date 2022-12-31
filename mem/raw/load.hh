1824201c114d77d933a60fd9449e100bc7ad5c8a
Mikael Simonsson <m@mikaelsimonsson.com>
2022-12-31 09:46:19 +0100

    mem::raw::load() should accept any octet pointer

--- mem/raw/load.hh
+++ mem/raw/load.hh
@@ -11,72 +11,71 @@ namespace snn::mem::raw
 {
     // ## Functions
 
-    // ### load - From char pointer
+    // ### load
 
-    template <typename Int>
-    [[nodiscard]] constexpr Int load(const char*) noexcept
-    {
-        static_assert(meta::always_false<Int>, "Not implemented.");
-        return 0;
-    }
+    // Load unsigned integral from _octet_ pointer.
 
-    template <>
-    [[nodiscard]] constexpr u16 load<u16>(const char* const data) noexcept
+    template <typename Int, octet Octet>
+    [[nodiscard]] constexpr Int load(const Octet* const data) noexcept
     {
-        u16 i = 0;
-        static_assert(sizeof(i) == 2);
-        if (std::is_constant_evaluated())
+        if constexpr (std::is_same_v<Int, u16>)
         {
-            i = to_byte(data[1]);
-            i <<= 8u;
-            i |= u16{to_byte(data[0])};
+            u16 i = 0;
+            static_assert(sizeof(i) == 2);
+            if (std::is_constant_evaluated())
+            {
+                i = static_cast<byte>(data[1]);
+                i <<= 8u;
+                i |= u16{static_cast<byte>(data[0])};
+            }
+            else
+            {
+                __builtin_memcpy(&i, data, sizeof(i));
+            }
+            return i;
+        }
+        else if constexpr (std::is_same_v<Int, u32>)
+        {
+            u32 i = 0;
+            static_assert(sizeof(i) == 4);
+            if (std::is_constant_evaluated())
+            {
+                i = static_cast<byte>(data[0]);
+                i |= u32{static_cast<byte>(data[1])} << 8u;
+                i |= u32{static_cast<byte>(data[2])} << 16u;
+                i |= u32{static_cast<byte>(data[3])} << 24u;
+            }
+            else
+            {
+                __builtin_memcpy(&i, data, sizeof(i));
+            }
+            return i;
+        }
+        else if constexpr (std::is_same_v<Int, u64>)
+        {
+            u64 i = 0;
+            static_assert(sizeof(i) == 8);
+            if (std::is_constant_evaluated())
+            {
+                i = static_cast<byte>(data[0]);
+                i |= u64{static_cast<byte>(data[1])} << 8u;
+                i |= u64{static_cast<byte>(data[2])} << 16u;
+                i |= u64{static_cast<byte>(data[3])} << 24u;
+                i |= u64{static_cast<byte>(data[4])} << 32u;
+                i |= u64{static_cast<byte>(data[5])} << 40u;
+                i |= u64{static_cast<byte>(data[6])} << 48u;
+                i |= u64{static_cast<byte>(data[7])} << 56u;
+            }
+            else
+            {
+                __builtin_memcpy(&i, data, sizeof(i));
+            }
+            return i;
         }
         else
         {
-            __builtin_memcpy(&i, data, sizeof(i));
+            static_assert(meta::always_false<Int>, "Not implemented.");
+            return 0;
         }
-        return i;
-    }
-
-    template <>
-    [[nodiscard]] constexpr u32 load<u32>(const char* const data) noexcept
-    {
-        u32 i = 0;
-        static_assert(sizeof(i) == 4);
-        if (std::is_constant_evaluated())
-        {
-            i = to_byte(data[0]);
-            i |= u32{to_byte(data[1])} << 8u;
-            i |= u32{to_byte(data[2])} << 16u;
-            i |= u32{to_byte(data[3])} << 24u;
-        }
-        else
-        {
-            __builtin_memcpy(&i, data, sizeof(i));
-        }
-        return i;
-    }
-
-    template <>
-    [[nodiscard]] constexpr u64 load<u64>(const char* const data) noexcept
-    {
-        u64 i = 0;
-        static_assert(sizeof(i) == 8);
-        if (std::is_constant_evaluated())
-        {
-            i = to_byte(data[0]);
-            i |= u64{to_byte(data[1])} << 8u;
-            i |= u64{to_byte(data[2])} << 16u;
-            i |= u64{to_byte(data[3])} << 24u;
-            i |= u64{to_byte(data[4])} << 32u;
-            i |= u64{to_byte(data[5])} << 40u;
-            i |= u64{to_byte(data[6])} << 48u;
-            i |= u64{to_byte(data[7])} << 56u;
-        }
-        else
-        {
-            __builtin_memcpy(&i, data, sizeof(i));
-        }
-        return i;
     }
 }
