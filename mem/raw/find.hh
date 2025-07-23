418a9a736b22c1446e6268a51d56c20d5c303d3b
Mikael Simonsson <m@mikaelsimonsson.com>
2025-07-23 09:15:30 +0200

    Add `mem::raw::find(...)` (`memmem`) function

--- mem/raw/find.hh
+++ mem/raw/find.hh
@@ -1,11 +1,11 @@
 // Copyright (c) 2025 Mikael Simonsson <https://mikaelsimonsson.com>.
 // SPDX-License-Identifier: BSL-1.0
 
-// # Find (`memchr`)
+// # Find (`memchr`, `memmem`)
 
 #pragma once
 
-#include "snn-core/core.hh"
+#include "snn-core/mem/raw/is_equal.hh"
 
 namespace snn::mem::raw
 {
@@ -60,8 +60,53 @@ namespace snn::mem::raw
         }
         else
         {
+            SNN_DIAGNOSTIC_PUSH
+            SNN_DIAGNOSTIC_IGNORE_UNSAFE_BUFFER_USAGE_IN_LIBC_CALL
+
             return static_cast<T*>(__builtin_memchr(haystack_data.get(), force_unsigned(needle),
                                                     haystack_size.get()));
+
+            SNN_DIAGNOSTIC_POP
         }
+    }
+
+    template <octet T, octet U>
+        requires same_as<const T, const U>
+    [[nodiscard]] constexpr T* find(const not_null<T*> haystack_data,
+                                    const byte_size<usize> haystack_size,
+                                    const not_null<U*> needle_data,
+                                    const not_zero<byte_size<usize>> needle_size) noexcept
+    {
+        if (haystack_size.get() < needle_size.get().get())
+        {
+            return nullptr;
+        }
+
+        SNN_DIAGNOSTIC_PUSH
+        SNN_DIAGNOSTIC_IGNORE_UNSAFE_BUFFER_USAGE
+
+        const auto needle_prefix = needle_data.get()[0];
+        auto* cur                = haystack_data.get();
+        auto* const last         = cur + (haystack_size.get() - needle_size.get().get() + 1);
+        snn_should(cur < last);
+        do
+        {
+            cur = mem::raw::find(not_null{cur}, byte_size{to_usize(last - cur)}, needle_prefix);
+            if (cur == nullptr)
+            {
+                return nullptr;
+            }
+
+            if (mem::raw::is_equal(not_null{cur}, needle_data, needle_size.get()))
+            {
+                return cur;
+            }
+
+            ++cur;
+        } while (cur < last);
+
+        SNN_DIAGNOSTIC_POP
+
+        return nullptr;
     }
 }
--- mem/raw/find.test.cc
+++ mem/raw/find.test.cc
@@ -7,7 +7,7 @@
 #include "snn-core/unittest.hh"
 #include "snn-core/random/number.hh"
 #include "snn-core/random/string.hh"
-#include <cstring> // memchr
+#include <string.h> // memchr, memmem
 
 namespace snn::app
 {
@@ -15,11 +15,33 @@ namespace snn::app
     {
         constexpr bool example()
         {
-            const array<byte, 4> a{182, 0, 34, 182};
-            snn_require(mem::raw::find(a.data(), a.byte_size(), byte{1}) == nullptr);
-            snn_require(mem::raw::find(a.data(), a.byte_size(), byte{182}) == &a.get<0>());
-            snn_require(mem::raw::find(a.data(), a.byte_size(), byte{0}) == &a.get<1>());
-            snn_require(mem::raw::find(a.data(), a.byte_size(), byte{34}) == &a.get<2>());
+            // Find a single byte (`memchr`).
+            {
+                const array<byte, 4> a{182, 0, 34, 182};
+                snn_require(mem::raw::find(a.data(), a.byte_size(), byte{1}) == nullptr);
+                snn_require(mem::raw::find(a.data(), a.byte_size(), byte{182}) == &a.get<0>());
+                snn_require(mem::raw::find(a.data(), a.byte_size(), byte{0}) == &a.get<1>());
+                snn_require(mem::raw::find(a.data(), a.byte_size(), byte{34}) == &a.get<2>());
+            }
+
+            // Find a byte substring (`memmem`).
+            {
+                const array<byte, 4> a{182, 0, 34, 182};
+                snn_require(mem::raw::find(a.data(), a.byte_size(), a.data(),
+                                           not_zero{a.byte_size()}) == &a.get<0>());
+            }
+            {
+                const array<byte, 4> haystack{182, 0, 34, 182};
+                const array<byte, 2> needle{34, 182};
+                snn_require(mem::raw::find(haystack.data(), haystack.byte_size(), needle.data(),
+                                           not_zero{needle.byte_size()}) == &haystack.get<2>());
+            }
+            {
+                const array<byte, 4> haystack{182, 0, 34, 182};
+                const array<byte, 3> needle{34, 182, 182};
+                snn_require(mem::raw::find(haystack.data(), haystack.byte_size(), needle.data(),
+                                           not_zero{needle.byte_size()}) == nullptr);
+            }
 
             return true;
         }
@@ -28,18 +50,28 @@ namespace snn::app
         {
             {
                 array<i8, 4> a{-128, 0, 34, -128};
+
                 snn_require(mem::raw::find(a.writable(), a.byte_size(), i8{1}) == nullptr);
                 snn_require(mem::raw::find(a.writable(), a.byte_size(), i8{-128}) == &a.get<0>());
                 snn_require(mem::raw::find(a.writable(), a.byte_size(), i8{0}) == &a.get<1>());
                 snn_require(mem::raw::find(a.writable(), a.byte_size(), i8{34}) == &a.get<2>());
+
+                array<i8, 2> n{-128, 0};
+                snn_require(mem::raw::find(a.writable(), a.byte_size(), n.data(),
+                                           not_zero{n.byte_size()}) == &a.get<0>());
             }
             {
                 array<char, 5> a{'a', '\0', to_char(174), '\n', '\0'};
+
                 snn_require(mem::raw::find(a.data(), a.byte_size(), ' ') == nullptr);
                 snn_require(mem::raw::find(a.data(), a.byte_size(), 'a') == &a.get<0>());
                 snn_require(mem::raw::find(a.data(), a.byte_size(), '\0') == &a.get<1>());
                 snn_require(mem::raw::find(a.data(), a.byte_size(), to_char(0xAE)) == &a.get<2>());
                 snn_require(mem::raw::find(a.data(), a.byte_size(), '\n') == &a.get<3>());
+
+                array<char, 3> n{'\0', to_char(174), '\n'};
+                snn_require(mem::raw::find(a.writable(), a.byte_size(), n.data(),
+                                           not_zero{n.byte_size()}) == &a.get<1>());
             }
 
             return true;
@@ -47,16 +79,18 @@ namespace snn::app
 
         bool test_find_rnd()
         {
-            for (loop::count lc{100}; lc--;)
+            for (loop::count lc{10}; lc--;)
             {
-                const auto haystack = random::string(random::number<usize>(0, 50));
+                const auto haystack = random::string(random::number<usize>(0, 10'000));
+
+                // Find a single byte (`memchr`).
 
                 if (haystack)
                 {
                     const char needle = haystack.back(assume::not_empty);
 
-                    auto* p = static_cast<const char*>(
-                        std::memchr(haystack.begin(), to_byte(needle), haystack.size()));
+                    auto* p = static_cast<const char*>(::memchr(haystack.begin(), to_byte(needle),
+                                                                haystack.size()));
                     snn_require(p != nullptr);
                     snn_require(mem::raw::find(haystack.data(), haystack.byte_size(), needle) == p);
                     snn_require(mem::raw::detail::find_slow(haystack.data(), haystack.byte_size(),
@@ -64,27 +98,53 @@ namespace snn::app
                 }
 
                 {
-                    auto* p = static_cast<const char*>(std::memchr(haystack.begin(), '\n',
-                                                                   haystack.size()));
+                    auto* p =
+                        static_cast<const char*>(::memchr(haystack.begin(), '\n', haystack.size()));
                     snn_require(mem::raw::find(haystack.data(), haystack.byte_size(), '\n') == p);
                     snn_require(mem::raw::detail::find_slow(haystack.data(), haystack.byte_size(),
                                                             '\n') == p);
                 }
                 {
-                    auto* p = static_cast<const char*>(std::memchr(haystack.begin(), '\0',
-                                                                   haystack.size()));
+                    auto* p =
+                        static_cast<const char*>(::memchr(haystack.begin(), '\0', haystack.size()));
                     snn_require(mem::raw::find(haystack.data(), haystack.byte_size(), '\0') == p);
                     snn_require(mem::raw::detail::find_slow(haystack.data(), haystack.byte_size(),
                                                             '\0') == p);
                 }
                 {
-                    auto* p = static_cast<const char*>(std::memchr(haystack.begin(), 0xFF,
-                                                                   haystack.size()));
+                    auto* p =
+                        static_cast<const char*>(::memchr(haystack.begin(), 0xFF, haystack.size()));
                     snn_require(mem::raw::find(haystack.data(), haystack.byte_size(),
                                                to_char(0xFF)) == p);
                     snn_require(mem::raw::detail::find_slow(haystack.data(), haystack.byte_size(),
                                                             to_char(0xFF)) == p);
                 }
+
+                // Find a byte substring (`memmem`).
+
+#if defined(__FreeBSD__) || defined(__linux__)
+                if (haystack)
+                {
+                    const auto needle = haystack.view_offset(-20);
+                    snn_require(needle);
+
+                    auto* p = static_cast<const char*>(::memmem(haystack.begin(), haystack.size(),
+                                                                needle.begin(), needle.size()));
+                    snn_require(p != nullptr);
+                    snn_require(mem::raw::find(haystack.data(), haystack.byte_size(), needle.data(),
+                                               not_zero{needle.byte_size()}) == p);
+                }
+
+                {
+                    const auto needle = random::string(random::number<usize>(1, 3));
+                    snn_require(needle);
+
+                    auto* p = static_cast<const char*>(::memmem(haystack.begin(), haystack.size(),
+                                                                needle.begin(), needle.size()));
+                    snn_require(mem::raw::find(haystack.data(), haystack.byte_size(), needle.data(),
+                                               not_zero{needle.byte_size()}) == p);
+                }
+#endif
             }
 
             return true;
