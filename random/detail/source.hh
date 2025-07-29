b1bbe72c98f8d62c0dbf308aabec5af349353488
Mikael Simonsson <m@mikaelsimonsson.com>
2025-07-29 10:48:19 +0200

    Use `arc4random` as random source on Linux
    `arc4random` is available in glibc since 2.36 (2022).

--- random/detail/source.hh
+++ random/detail/source.hh
@@ -1,17 +1,10 @@
-// Copyright (c) 2022 Mikael Simonsson <https://mikaelsimonsson.com>.
+// Copyright (c) 2025 Mikael Simonsson <https://mikaelsimonsson.com>.
 // SPDX-License-Identifier: BSL-1.0
 
 #pragma once
 
 #include "snn-core/array_view.hh"
-#if defined(__linux__)
-    #include "snn-core/exception.hh"
-    #include "snn-core/math/common.hh"
-    #include "snn-core/system/error.hh"
-    #include <unistd.h> // getentropy
-#else
-    #include <cstdlib> // arc4random, arc4random_buf
-#endif
+#include <stdlib.h> // arc4random, arc4random_buf
 
 // arc4random(3) never fails and uses ChaCha20 as of FreeBSD 12.
 
@@ -21,31 +14,12 @@ namespace snn::random::detail
     {
         static void fill(strview buffer)
         {
-#if defined(__linux__)
-            while (buffer)
-            {
-                // getentropy() can only fill 256 bytes.
-                const usize chunk_size = math::min(buffer.size(), 256);
-                if (::getentropy(buffer.writable().get(), chunk_size) != 0)
-                {
-                    throw_or_abort(error_code{errno, system::error_category});
-                }
-                buffer.drop_front_n(chunk_size);
-            }
-#else
             ::arc4random_buf(buffer.writable().get(), buffer.size());
-#endif
         }
 
         static u32 number()
         {
-#if defined(__linux__)
-            u32 n;
-            fill(strview{not_null{reinterpret_cast<char*>(&n)}, sizeof(n)});
-            return n;
-#else
             return ::arc4random();
-#endif
         }
     };
 }
