a0911c37ab9e61836a3819e7de40f41482ff82a7
Mikael Simonsson <m@mikaelsimonsson.com>
2025-08-05 09:32:33 +0200

    Split `fmt::fn::format` into two function objects
    `fmt::fn::format` should have been named `fmt::fn::format_append`,
    rename and add `fmt::fn::format`.

--- fmt/fn/format.hh
+++ fmt/fn/format.hh
@@ -1,7 +1,7 @@
-// Copyright (c) 2022 Mikael Simonsson <https://mikaelsimonsson.com>.
+// Copyright (c) 2025 Mikael Simonsson <https://mikaelsimonsson.com>.
 // SPDX-License-Identifier: BSL-1.0
 
-// # Format and append
+// # Format
 
 #pragma once
 
@@ -11,30 +11,55 @@ namespace snn::fmt::fn
 {
     // ## Classes
 
-    // ### format
+    // ### `format`
 
-    template <any_strcore Str>
-        requires(!std::is_const_v<Str>)
+    template <any_strcore Str = str>
     class format final
     {
       public:
-        // #### Constructors
+        // #### Explicit constructors
+
+        constexpr explicit format(const cstrview string) noexcept
+            : string_{string}
+        {
+        }
+
+        // #### Operations
+
+        template <typename... Args>
+        [[nodiscard]] constexpr auto operator()(const Args&... args) const
+        {
+            return fmt::format<Str>(string_, args...);
+        }
+
+      private:
+        cstrview string_;
+    };
+
+    // ### `format_append`
+
+    template <any_strcore Str>
+        requires(!std::is_const_v<Str>)
+    class format_append final
+    {
+      public:
+        // #### Explicit constructors
 
         template <same_as<const char> ConstChar, usize N>
-        constexpr explicit format(ConstChar (&string)[N], Str& append_to) noexcept
+        constexpr explicit format_append(ConstChar (&string)[N], Str& append_to) noexcept
             : string_{string},
               append_to_{append_to}
         {
         }
 
-        constexpr explicit format(const cstrview string, Str& append_to,
-                                  assume::no_overlap_t) noexcept
+        constexpr explicit format_append(const cstrview string, Str& append_to,
+                                         assume::no_overlap_t) noexcept
             : string_{string},
               append_to_{append_to}
         {
         }
 
-        // #### Operator()
+        // #### Operations
 
         template <typename... Args>
         constexpr void operator()(const Args&... args) const
--- fmt/fn/format.test.cc
+++ fmt/fn/format.test.cc
@@ -1,9 +1,13 @@
-// Copyright (c) 2022 Mikael Simonsson <https://mikaelsimonsson.com>.
+// Copyright (c) 2025 Mikael Simonsson <https://mikaelsimonsson.com>.
 // SPDX-License-Identifier: BSL-1.0
 
 #include "snn-core/fmt/fn/format.hh"
 
 #include "snn-core/unittest.hh"
+#include "snn-core/algo/is_equal.hh"
+#include "snn-core/range/generate.hh"
+#include "snn-core/range/view/take.hh"
+#include "snn-core/range/view/transform.hh"
 
 namespace snn::app
 {
@@ -12,18 +16,42 @@ namespace snn::app
         constexpr bool test_format()
         {
             {
+                fmt::fn::format f{"{}, {}, {}\n"};
+                snn_require(f('a', 123, "abc") == "a, 123, abc\n");
+                snn_require(f(1, 2, 3) == "1, 2, 3\n");
+                static_assert(std::is_same_v<decltype(f(1, 2, 3)), str>);
+            }
+            {
+                fmt::fn::format<strbuf> f{"{}: {}"};
+                snn_require(f('a', 'b') == "a: b");
+                snn_require(f(7, 8) == "7: 8");
+                static_assert(std::is_same_v<decltype(f(7, 8)), strbuf>);
+            }
+            {
+                snn_require(algo::is_equal(range::generate{fn::incrementing{1'000}} |
+                                               range::v::take{3} |
+                                               range::v::transform{fmt::fn::format{"{:d }"}},
+                                           init_list<cstrview>{"1 000", "1 001", "1 002"}));
+            }
+
+            return true;
+        }
+
+        constexpr bool test_format_append()
+        {
+            {
                 str out;
-                fmt::fn::format format{"{}, {}, {}\n", out};
-                format('a', 123, "abc");
-                format(1, 2, 3);
+                fmt::fn::format_append f{"{}, {}, {}\n", out};
+                f('a', 123, "abc");
+                f(1, 2, 3);
                 snn_require(out == "a, 123, abc\n1, 2, 3\n");
             }
             {
-                str out;
+                strbuf out;
                 constexpr cstrview format_string{"{}: {}, "};
-                fmt::fn::format format{format_string, out, assume::no_overlap};
-                format('a', 'b');
-                format(7, 8);
+                fmt::fn::format_append f{format_string, out, assume::no_overlap};
+                f('a', 'b');
+                f(7, 8);
                 snn_require(out == "a: b, 7: 8, ");
             }
 
@@ -37,5 +65,6 @@ namespace snn
     void unittest()
     {
         snn_static_require(app::test_format());
+        snn_static_require(app::test_format_append());
     }
 }
